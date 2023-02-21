4a44bca7368e3ddea48b31b7dcbbeb4ceffcb972
siduck <siduck@tutanota.com>
2023-02-21 16:36:35 +0530

    generate desktop entry via install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ cp -r ./keystrokes/config/.bubblyrc "$HOME/.config"
 install_dir=$(pwd)
 mv "$install_dir" "$HOME/.local/share"
 
+# create desktop file
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Bubbly
+Comment=Screencast keys & generate on-screen chat bubbles
+Exec=/home/$USER/.local/share/bubbly/start.sh
+Icon=preferences-desktop-keyboard-shortcuts
+Categories=Utility;" >"$HOME/.local/share/applications/bubbly.desktop"
+
 # uses xinput to get active keyboard ( device ID )
 echo "press any key to get device name"
 
